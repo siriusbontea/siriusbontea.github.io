35b1d1bd78435aa474febaf224e346b21899e408
Sirius T. Bontea <sirius.bontea@uky.edu>
2022-03-25 08:11:53 -0600

    added UKy marker

--- MAP672/finalproject/site-data/map.js
+++ MAP672/finalproject/site-data/map.js
@@ -15,34 +15,43 @@ PROJCS["NAD_1983_HARN_StatePlane_North_Dakota_South_FIPS_3302",
     UNIT["Meter",1],
     AUTHORITY["EPSG","102321"]]
  */
- 
- var naprj = {
-     epsg: 'EPSG:102321',
-     def: '+proj=lcc +lat_1=46.18333333333333 +lat_2=47.48333333333333 +lat_0=45.66666666666666 +lon_0=-100.5 +x_0=600000 +y_0=0 +ellps=GRS80 +units=m no_defs',
-     resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
-     origin: [0, 0]
- }
 
- var crs = new L.Proj.CRS(naprj.epsg, naprj.def, {
-     resolutions: naprj.resolutions,
-     origin: naprj.origin
- })
+var naprj = {
+    epsg: 'EPSG:102321',
+    def: '+proj=lcc +lat_1=46.18333333333333 +lat_2=47.48333333333333 +lat_0=45.66666666666666 +lon_0=-100.5 +x_0=600000 +y_0=0 +ellps=GRS80 +units=m no_defs',
+    resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
+    origin: [0, 0]
+}
 
- var options = {
-     crs: crs,
+var crs = new L.Proj.CRS(naprj.epsg, naprj.def, {
+    resolutions: naprj.resolutions,
+    origin: naprj.origin
+})
+
+var options = {
+    crs: crs,
     //  center: [38.0386253, -84.5042394], // University of Kentucky - Department of Geography, Lexington, Kentucky
     //  center: [36.6963788, -84.5468082], // Bob Ross Rd, Stearns, Kentucky
     //  center: [29.0274313, -80.8953009], // Bob Ross Art Workshop, New Smyrna Beach, Florida
     //  center: [39.9989898, -102.0508567], // CO KS NE Triple Point
-     center: [47.1152105,-101.3031364], // Center, North Dakota - The Geographical Centre of North America!
-     zoom: 1,
-     zoomSnap: .5,
-     zoomDelta: 0.2,
-     zoomControl: false
- }
- var map = L.map('map', options);
- map.attributionControl.setPrefix('');
+    center: [47.1152105, -101.3031364], // Center, North Dakota - The Geographical Centre of North America!
+    zoom: 1,
+    zoomSnap: .5,
+    zoomDelta: 0.2,
+    zoomControl: false
+}
+var map = L.map('map', options);
+map.attributionControl.setPrefix('');
 
 //  L.control.zoom({
 //      position: 'bottomright'
 //  }).addTo(map);
+
+var UKyIcon = L.icon({
+    iconUrl: 'site-data/graphics/Kentucky_Wildcats_logo.svg',
+    iconSize: [30, 33], // size of the icon
+    iconAnchor: [5,10], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([38.0386253, -84.5042394], {icon: UKyIcon}).addTo(map);
