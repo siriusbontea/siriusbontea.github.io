a1a77bedb9791f81e80aaeb7efc97b6f4fb4f7ee
Sirius T. Bontea <sirius.bontea@uky.edu>
2022-03-24 23:45:31 -0600

    meta data added, partial map

---
+++ MAP672/final-project/site-data/map.js
@@ -0,0 +1,47 @@
+/*
+PROJCS["NAD_1983_HARN_StatePlane_North_Dakota_South_FIPS_3302",
+    GEOGCS["GCS_North_American_1983_HARN",
+        DATUM["NAD83_High_Accuracy_Regional_Network",
+            SPHEROID["GRS_1980",6378137,298.257222101]],
+        PRIMEM["Greenwich",0],
+        UNIT["Degree",0.017453292519943295]],
+    PROJECTION["Lambert_Conformal_Conic_2SP"],
+    PARAMETER["False_Easting",600000],
+    PARAMETER["False_Northing",0],
+    PARAMETER["Central_Meridian",-100.5],
+    PARAMETER["Standard_Parallel_1",46.18333333333333],
+    PARAMETER["Standard_Parallel_2",47.48333333333333],
+    PARAMETER["Latitude_Of_Origin",45.66666666666666],
+    UNIT["Meter",1],
+    AUTHORITY["EPSG","102321"]]
+ */
+ 
+ var naprj = {
+     epsg: 'EPSG:102321',
+     def: '+proj=lcc +lat_1=46.18333333333333 +lat_2=47.48333333333333 +lat_0=45.66666666666666 +lon_0=-100.5 +x_0=600000 +y_0=0 +ellps=GRS80 +units=m no_defs',
+     resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
+     origin: [0, 0]
+ }
+
+ var crs = new L.Proj.CRS(naprj.epsg, naprj.def, {
+     resolutions: naprj.resolutions,
+     origin: naprj.origin
+ })
+
+ var options = {
+     crs: crs,
+     center: [38.0386253, -84.5042394], // University of Kentucky - Department of Geography, Lexington, Kentucky
+    //  center: [36.6963788, -84.5468082], // Bob Ross Rd, Stearns, Kentucky
+    //  center: [29.0274313, -80.8953009], // Bob Ross Art Workshop, New Smyrna Beach, Florida
+    //  center: [39.9989898, -102.0508567], // CO KS NE Triple Point
+    //  center: [47.1152105,-101.3031364], // Center, North Dakota - The Geographical Centre of North America!
+     zoom: 2,
+     zoomSnap: .5,
+     zoomDelta: 0.2,
+     zoomControl: false
+ }
+ var map = L.map('map', options);
+
+//  L.control.zoom({
+//      position: 'bottomright'
+//  }).addTo(map);
