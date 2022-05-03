7e4239ace93d22a2b52c60971eefa52ff0735f0a
sirius.bontea <sirius.bontea@uky.edu>
2022-05-03 18:11:32 -0500

    added more portfolio links

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+ */
 //
 // Scripts
 // 
@@ -51,45 +51,63 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
     const hexcontent = {
-        count: 5,
+        count: 8,
         0: {
             href: 'kentucky-owls',
-            img: 'project1.png',
+            img: 'kentucky-owls.png',
             h1: 'Kentucky\'s Owls',
             p: 'Mapping native and visiting Owls in Kentucky'
         },
         1: {
             href: 'snowy-owls',
-            img: 'project2.png',
+            img: 'snowy-owls.png',
             h1: 'Snowy Owls',
             p: 'The North American Snowy Owl'
         },
         2: {
-            href: 'electrical-power',
-            img: 'project3.png',
-            h1: 'Electrical Power',
-            p: '"green" energy in the United States'
-        },
-        3: {
-            href: 'alaska-wages',
-            img: 'project4.png',
-            h1: 'Alaska Wages',
-            p: 'Choropleth map showing blue-collar wages'
-        },
-         4: {
             href: 'karate',
-            img: 'project5.png',
+            img: 'okinawa-karate.png',
             h1: 'Okinawa',
             p: 'The birthplace of karate'
         },
+        3: {
+            href: 'alaska-wages',
+            img: 'blue-collar-alaska.png',
+            h1: 'Alaska Wages',
+            p: 'Choropleth map showing blue-collar wages'
+        },
+        4: {
+            href: 'electrical-power',
+            img: 'power-plants.png',
+            h1: 'Electrical Power',
+            p: '"green" energy in the United States'
+        },
         5: {
+            href: 'round-earth',
+            img: 'round-earth.png',
+            h1: 'Round Earth',
+            p: 'Round like an oblong spheroid'
+        },
+        6: {
+            href: 'new-orleans',
+            img: 'new-orleans.png',
+            h1: 'New Orleans',
+            p: 'Mapping Points of Interest'
+        },
+        7: {
+            href: 'prison-town',
+            img: 'leavenworth-donuts.png',
+            h1: 'Prison Town',
+            p: 'Fort Leavenworth, Leavenworth, Lansing Prisons'
+        },
+        8: {
             img: 'project-placeholder.png',
             h1: 'Project Placeholder',
             p: 'Placeholder for mapping project'
         },
 
     }
-    document.querySelectorAll('.hexIn').forEach(function(item, index) {
+    document.querySelectorAll('.hexIn').forEach(function (item, index) {
         let props
         let link = ''
         let count = hexcontent.count
@@ -99,7 +117,7 @@ window.addEventListener('DOMContentLoaded', event => {
             props = hexcontent[index];
             link = `<a href="https://siriusbontea.github.io/${props.href}">${props.p}</a>`;
         }
-    
+
         const place = ` <div class="hexLink">
         <div class="img" style="background-image:url(assets/img/${props.img});"></div>
         <h1 id="hextitle">${props.h1}</h1>
@@ -107,8 +125,8 @@ window.addEventListener('DOMContentLoaded', event => {
         </a>`
 
         item.innerHTML = place;
-        item.addEventListener('mouseover', function(e) {
-          console.log(item);
+        item.addEventListener('mouseover', function (e) {
+            console.log(item);
         });
     });
 
